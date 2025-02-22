02c958e2afe3b83ca4e04c998b0f0b4dc0b8779c
Bishesh2527 <bisheshkhadka2527@gmail.com>
2025-02-22 15:20:23 +0545

    Practice py

---
+++ new.py
@@ -0,0 +1,37 @@
+# check = int(input("Enter a number to check odd or even: "))
+# if (check%2 == 0):
+#     print("Even")
+# else:
+#     print("Odd")
+    
+# number1 = int(input("Enter a 1st number: "))
+# number2 = int(input("Enter a 2nd number: "))
+# number3 = int(input("Enter a 3rd number: "))
+# enter_number = (number1, number2, number3)
+
+# if(number1>number2 and  number1>number3):
+#     print( "Number 1: " , number1 , "is the greatest number")
+# elif(number2>number1 and number2 > number3):
+#     print("Number 2: " , number2 , "is the greatest number")
+# elif(number3>number2 and number3> number1):
+#     print("Number 3: " , number3 , "is the greatest number")
+
+# number = int(input("Enter a number to check ? : "))
+# if (number%7 == 0):
+#     print("yes it is a multiple of 7")
+# else:
+#     print("no the number isnt multiple of 7")
+
+# a = int(input("Enter 1st number: "))
+# b = int(input("Enter 2nd number: "))
+# c = int(input("Enter 3rd number: "))
+# d = int(input("Enter 4th number: "))
+
+# if (a>b and b>c and c > d):
+#     print("a:" ,a)
+# elif(b>c and c>d):
+#     print("b:", b)
+# elif(c > d):
+#     print("c:" ,c)
+# else:
+#     print("d", d)
